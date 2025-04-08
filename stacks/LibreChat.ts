29bb68b77c4433a4dc3bd1e7d65a65437abbbf91
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2025-04-08 22:20:04 +0200

    Added certificate to Libre Chat stack.

--- stacks/LibreChat.ts
+++ stacks/LibreChat.ts
@@ -1,4 +1,7 @@
+import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager';
+import * as constants from './constants';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as sst from 'sst/constructs';
 import { setStandardTags } from './tags';
 
@@ -8,6 +11,16 @@ export function LibreChat({ stack }: sst.StackContext) {
   const vpc = new ec2.Vpc(stack, 'Vpc', {
     maxAzs: 1,
     natGateways: 1,
+  });
+
+  const hostedZone = route53.HostedZone.fromLookup(stack, 'HostedZone', {
+    domainName: constants.hostedDomainName,
+  });
+  const domainName = `${stack.stage}.${constants.hostedDomainName}`;
+
+  const certificate = new certificatemanager.Certificate(stack, 'Certificate', {
+    domainName: domainName,
+    validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
   });
 
   const instance = new ec2.Instance(stack, 'LibreChatInstance', {
@@ -27,5 +40,6 @@ export function LibreChat({ stack }: sst.StackContext) {
   stack.addOutputs({
     vpcId: vpc.vpcId,
     instanceId: instance.instanceId,
+    certificateArn: certificate.certificateArn,
   });
 }
---
+++ stacks/constants.ts
@@ -0,0 +1,1 @@
+export const hostedDomainName = 'aicw.mediasciencelab.com';
