9ac07562ba34ffdf96135ccba3673481a4de3af2
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2025-04-16 17:16:10 +0200

    Create a secrets accessing policy.

--- stacks/LibreChatStatic.ts
+++ stacks/LibreChatStatic.ts
@@ -1,4 +1,5 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as sst from 'sst/constructs';
 import { setStandardTags } from './tags';
@@ -14,13 +15,36 @@ export function LibreChatStatic({ stack }: sst.StackContext) {
 
   const kmsKey = new kms.Key(this, 'KmsKey');
 
+  const ssmPolicyStatement = new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    actions: [
+      'ssm:GetParameter',
+      'ssm:GetParameters',
+      'ssm:GetParameterHistory',
+      'ssm:GetParametersByPath',
+      'ssm:DescribeParameters',
+    ],
+    resources: [`arn:aws:ssm:*:*:mediasci/aicw/librechat/${stack.stage}/*`],
+  });
+
+  const kmsKeyDecryptionPolicyStatement = new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    actions: ['kms:Decrypt'],
+    resources: [kmsKey.keyArn],
+  });
+
+  const secretsPolicy = new iam.ManagedPolicy(stack, 'SecretsPolicy', {
+    statements: [ssmPolicyStatement, kmsKeyDecryptionPolicyStatement],
+  });
+
   // Export values from cloudformation template.
   stack.addOutputs({
     keyPairName: keyPair.keyPairName,
     keyPairPrivateKeyParameter: keyPair.privateKey.parameterName,
     kmsKeyId: kmsKey.keyId,
     libreChatIpAddress: libreChatIpAddress.attrPublicIp,
+    secretsPolicyArn: secretsPolicy.managedPolicyArn,
   });
 
-  return { keyPair, kmsKey, libreChatIpAddress };
+  return { keyPair, kmsKey, libreChatIpAddress, secretsPolicy };
 }
